5175e7679450c57f1b15e0ccc9d1928293ccd246
StrikeZ <fpbrauner@gmail.com>
2022-08-25 15:53:08 +0200

    Historie med loops og arrays

--- 0_HelloWorld/Program.cs
+++ 0_HelloWorld/Program.cs
@@ -2,7 +2,7 @@
 Console.Title = "Matrix";
 Console.ForegroundColor = ConsoleColor.DarkGreen;
 
-void text(string text, int time = 35)
+void text(string text, int time = 20)
 {
     foreach (char c in text)
     {
@@ -21,19 +21,67 @@ text("\nDo you wanna go on an adventure? \n");
 string answer = Console.ReadLine();
 
 if (answer == "no")
-{
-    text("Okay then");
-    Thread.Sleep(500);
-}
+    {
+        text("Okay then");
+        Thread.Sleep(500);
+    }
 
 else if (answer == "yes")
-{
-    text("\nLet's go deeper then");
-}
+    {
+        text("\nLet's go deeper then");
+    }
 else
+    {
+        text("okay");
+    }
+
+text("\nSo you want to know about the things keeping an eye on you?\n");
+
+string answer1 = Console.ReadLine();
+
+if (answer1 == "no")
+    {
+        text("Well good luck then...\n");
+        Thread.Sleep(500);
+    }
+
+else if (answer1 == "yes")
+    {
+        text("\nWell let's go into The Matrix");
+        Console.Clear();
+    }
+    
+text("Hello again.\n");
+Console.ReadLine();
+
+string[] places = {"Google", "Meta", "Twitter"};
+
+for (int i = 0; i < places.Length; i++)
 {
-    text("okay");
+    int rank = i + 1;
+    Console.WriteLine(rank + ". "+ places[i] );
 }
 
-text("\So you want to know about the things keeping an eye on you?");
-Console.ReadLine();
+string[] yourplaces = new string[3];
+
+Console.WriteLine("Type the worst tech companies that spies on you");
+
+for (int i = 0; i < yourplaces.length; i++)
+{
+    yourplaces[i] = Console.ReadLine();
+}
+
+Console.WriteLine("\nJust for fun, here they are alphabetically: ");
+
+Array.Sort(yourplaces);
+
+for (int i = 0; i < yourplaces.length; i++)
+{
+    Console.WriteLine(yourplaces[i]);    
+}
+
+
+
+
+
+
